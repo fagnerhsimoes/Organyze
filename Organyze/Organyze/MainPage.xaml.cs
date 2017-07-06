f685491b03ca26df75f9773f07dc63814819936a
Fagner Henrique Simões <fagnerhsimoes@gmail.com>
2017-07-06 20:56:24 -0300

    Adicionar arquivos de projeto.

---
+++ Organyze/Organyze/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Organyze
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
